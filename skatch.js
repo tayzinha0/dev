c82dd637d04b0e252a1c32c946675614c6ac0f8a
tayzinha <00001113943051sp@al.educacao.sp.gov.br>
2024-08-26 08:06:22 -0300

    adicionando arquivos js e css

---
+++ skatch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("white");
+    
+  stroke("blue")
+      fill("red")
+      rect(0, 250, 20 , 35)
+  }
+  
